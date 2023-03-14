e2d816fe8ea7d2e24e497c2e443e5e8afb88d3cf
Radhe Shyam Lodhi <radheshyamlodhi64@gmail.com>
2023-03-14 23:46:16 +0530

    Add feed service Unit test

--- src/test/java/com/newsfeed/service/NotificationServiceTest.java
+++ src/test/java/com/newsfeed/service/NotificationServiceTest.java
@@ -4,10 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class NotificationServiceTest {
-
     @BeforeEach
     void setUp() {
     }
